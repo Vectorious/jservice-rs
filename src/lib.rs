c08a1397f85ce18a36248f04482d5547e2bb5766
Brandon King <wektorious@gmail.com>
2016-05-22 23:09:29 -0700

    Fix gh-pages deploy location

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 //! # jservice-rs
 //!
 //! jservice-rs is an API wrapper for [jService](http://jservice.io/).
+#![doc(html_root_url = "https://vectorious.github.io/jservice-rs/")]
 
 extern crate serde;
 extern crate serde_json;
