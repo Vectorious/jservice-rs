8c525317c49c5d598116dff00b119ea7b74e85ba
Brandon King <wektorious@gmail.com>
2016-05-22 22:46:21 -0700

    Fix test

--- tests/mod.rs
+++ tests/mod.rs
@@ -5,7 +5,7 @@ fn deserialize_clue() {
     extern crate serde_json;
 
     let bytes = r#"{"id":1,"answer":"Election Day","question":"1st Tuesday after the 1st Monday in November","value":100,"airdate":"1985-02-08T12:00:00.000Z","created_at":"2014-02-11T22:47:18.786Z","updated_at":"2014-02-11T22:47:18.786Z","category_id":1,"game_id":null,"invalid_count":null,"category":{"id":1,"title":"politics","created_at":"2014-02-11T22:47:18.687Z","updated_at":"2014-02-11T22:47:18.687Z","clues_count":30}}"#;
-    let clue: Clue = serde_json::from_str(bytes).unwrap();
+    let clue: jservice::Clue = serde_json::from_str(bytes).unwrap();
     assert_eq!(clue.question, "1st Tuesday after the 1st Monday in November");
 }
 
