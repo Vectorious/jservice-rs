0045d6f8f7b949220da5712f49cba6f42a331b7b
Brandon King <wektorious@gmail.com>
2016-10-03 18:34:38 -0700

    Unbreak serde_codegen

--- build.rs
+++ build.rs
@@ -10,8 +10,5 @@ pub fn main() {
     let src = Path::new("src/lib.rs.in");
     let dst = Path::new(&out_dir).join("lib.rs");
 
-    let mut registry = syntex::Registry::new();
-
-    serde_codegen::register(&mut registry);
-    registry.expand("", &src, &dst).unwrap();
+    serde_codegen::expand(&src, &dst).unwrap();
 }
